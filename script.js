28c4bab7fc7a297154425c88ad1ccdfcc1cbf8e2
pachicodes <pachicarlson@gmail.com>
2019-07-25 08:03:23 -0400

    apanha que faz bem

---
+++ script.js
@@ -0,0 +1,14 @@
+let bill = document.querySelector('[data-bill]');
+let percent = document.querySelector('[data-percent]');
+let res = document.querySelector('[data-res]');
+
+function calcTip() {
+    if (bill.value <= 0 || percent.value <= 0) {
+        window.alert('Please enter the numbers dude.')
+    }
+let billNum = Number(bill.value);
+let percentNum = Number(percent.value);
+
+let tip = billNum * percentNum / 100;
+console.log(let)
+}
