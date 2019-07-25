a42bbae4f6fbf8d58a22e470df4b296894f4d924
pachicodes <pachicarlson@gmail.com>
2019-07-25 18:08:59 -0400

    Maybe done

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ let percent = document.querySelector('[data-percent]');
 let res = document.querySelector('[data-res]');
 
 function calcTip() {
+    let billNum = Number(bill.value);
+    let percentNum = Number(percent.value);
+    let tip = (billNum * percentNum) / 100;
+    let tots = billNum + tip;
+
     if (bill.value <= 0 || percent.value <= 0) {
         window.alert('Please enter the numbers dude.')
+    } else {
+        res.innerHTML = '';
+        res.innerHTML += `<p>Tip is $ ${tip}.</p>`
+        res.innerHTML += `<p>Bill total is $ ${tots}.`
     }
-let billNum = Number(bill.value);
-let percentNum = Number(percent.value);
 
-let tip = (billNum * percentNum) / 100;
-console.log(tip)
 }
